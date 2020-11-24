65c063f43b88792d1ffdd104ca093be3014f8e85
SuperWallaby <colton950901@gmail.com>
2020-11-24 17:32:51 +0900

    ..

--- components/editorjs/EditorJs.tsx
+++ components/editorjs/EditorJs.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { EditorConfig } from '@editorjs/editorjs';
+import EditorJs from 'react-editor-js';
+import { EditorJsProps } from 'react-editor-js/dist/EditorJs';
+import RER from "react-editorjs-renderer"
+interface IProp extends EditorConfig {
+}
 
-interface IProp { }
+export const Editor: React.FC<IProp> = (props) => {
+    const [editorTools, setTools] = useState<any>(undefined);
 
-export const EditorJS: React.FC<IProp> = () => {
-    return <div />;
+    useEffect(() => {
+        const fetch = async () => {
+            const editorTools = (await import('./tools.js'))
+            setTools(editorTools);
+        }
+
+        fetch()
+    }, [])
+
+
+    if (!editorTools) {
+        return <RER data={props.data} />
+    }
+
+    return <EditorJs {...props} tools={editorTools} />;
 };
 
-export default EditorJS;
+export default Editor;
