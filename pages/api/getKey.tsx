fb75d3e24f6deefbc16757292e553965412a1308
SuperWallaby <colton950901@gmail.com>
2020-11-24 18:54:14 +0900

    ...

--- pages/api/getKey.tsx
+++ pages/api/getKey.tsx
@@ -1,27 +1,27 @@
-var FroalaEditor = require('wysiwyg-editor-node-sdk');
+// var FroalaEditor = require('wysiwyg-editor-node-sdk');
 
-export default function handler(req, res) {
+// export default function handler(req, res) {
 
 
-    var configs = {
-        // The name of your bucket.
-        bucket: 'pink-editor-uploads',
-        // S3 region. If you are using the default us-east-1, it this can be ignored.
-        region: 'ap-northeast-2',
+//     var configs = {
+//         // The name of your bucket.
+//         bucket: 'pink-editor-uploads',
+//         // S3 region. If you are using the default us-east-1, it this can be ignored.
+//         region: 'ap-northeast-2',
 
-        // The folder where to upload the images.
-        keyStart: 'pink',
+//         // The folder where to upload the images.
+//         keyStart: 'pink',
 
-        // File access.
-        acl: 'public-read',
+//         // File access.
+//         acl: 'public-read',
 
-        // AWS keys.
-        accessKey: process.env.AWS_ACCESS_KEY,
-        secretKey: process.env.AWS_SECRET_KEY
-    }
+//         // AWS keys.
+//         accessKey: process.env.AWS_ACCESS_KEY,
+//         secretKey: process.env.AWS_SECRET_KEY
+//     }
 
-    var s3Hash = FroalaEditor.S3.getHash(configs);
-    console.log(s3Hash);
+//     var s3Hash = FroalaEditor.S3.getHash(configs);
+//     console.log(s3Hash);
 
-    res.end(JSON.stringify(s3Hash))
-}
+//     res.end(JSON.stringify(s3Hash))
+// }
