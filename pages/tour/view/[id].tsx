fc7ec6fbdf8a8fe89ad2ae43eaf49930c6b0b049
JANDA-Developers <#janda123>
2020-12-02 14:45:51 +0900

    //

--- pages/tour/view/[id].tsx
+++ pages/tour/view/[id].tsx
@@ -127,6 +127,8 @@ const TourDetail: React.FC<IProps> = ({ product }) => {
     setCount({ ...count })
   }
 
+  const fistDate = itinerary[0]?.date;
+
   useEffect(() => {
     const price =
       (count.adult * adult_price) +
@@ -178,7 +180,6 @@ const TourDetail: React.FC<IProps> = ({ product }) => {
               )}
             </ul>
             <div className="details_info_txt">
-              <i className="flaticon-flag-1" />
               <div dangerouslySetInnerHTML={{ __html: caution }} />
             </div>
           </div>
@@ -209,6 +210,10 @@ const TourDetail: React.FC<IProps> = ({ product }) => {
                         )}
                       </ul>
                     </td>
+                  </tr>
+                  <tr>
+                    <th className="smtitle bt_line">출발일</th>
+                    <td className="smtxt bt_line">{dayjs(fistDate).format("YYYY.MM.DD")}일</td>
                   </tr>
                   <tr>
                     <th className="smtitle bt_line">여행기간</th>
