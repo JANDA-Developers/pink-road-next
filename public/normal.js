9c1cfd705339b903f8dd24d4627dc11ce3337a38
ksh_dd <zocbdjf@gmail.com>
2020-12-02 11:37:16 +0900

    ㅇㅇㅇ

--- public/normal.js
+++ public/normal.js
@@ -3,7 +3,7 @@ $(window).scroll(function (e) {
     var scrollHeight = $(this).scrollTop();
     console.log(scrollHeight);
     if (scrollHeight < 670) {
-        console.log("670 이하");
+        console.log("670 이하"); 
         $("#gotop").addClass("animationHide");
     } else {
         $("#gotop").removeClass("animationHide");
