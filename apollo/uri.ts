7c450b38c0546660ba64d70f2fc028cd24e229ff
ksh_dd <zocbdjf@gmail.com>
2020-12-29 15:40:40 +0900

    ..

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,5 +1,5 @@
-const END_POINT = process.env.NEXT_PUBLIC_SERVER_URI + "/graphql";
-export const SERVER_URI:string = END_POINT || "";
+const END_POINT = process.env.NEXT_PUBLIC_PINK_SERVER_URI + "/graphql";
+export const SERVER_URI: string = END_POINT || "";
 // "http://localhost:4000/graphql"
 // 프로덕트: http://13.209.234.84:4000/graphql
 // 개발: http://3.35.164.252:4000/graphql
