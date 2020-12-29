c927bc7e72d1accd6d7ff3fa1e9a3a5178209779
JANDA-Developers <#janda123>
2020-12-29 16:00:49 +0900

    엔드포인트 수정 [로컬용]

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,4 +1,5 @@
-const END_POINT = process.env.NEXT_PUBLIC_SERVER_URI + "/graphql";
+console.log(process.env.NEXT_PUBLIC_SERVER_URI_LOCAL)
+const END_POINT = process.env.NEXT_PUBLIC_SERVER_URI_LOCAL + "/graphql";
 export const SERVER_URI:string = END_POINT || "";
 // "http://localhost:4000/graphql"
 // 프로덕트: http://13.209.234.84:4000/graphql
