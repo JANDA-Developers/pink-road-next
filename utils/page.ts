3e75c5e7e2d72b024f7f2927b06dec0464e93553
ksh_dd <zocbdjf@gmail.com>
2020-12-29 16:44:28 +0900

    .

--- utils/page.ts
+++ utils/page.ts
@@ -5,11 +5,11 @@ import { usePageInfo } from "../hook/usePageInfo";
 import { Fhomepage, Fpage, homepage } from "../types/api"
 import { TPageKeys } from "../types/interface";
 
-export const getQueryIndex = (inPageIndex:number,pageInfo:Fpage) => {
-    const {remainder,cntPerPage,totalPageSize} = pageInfo;
+export const getQueryIndex = (inPageIndex: number, pageInfo: Fpage) => {
+    const { remainder, cntPerPage, totalPageSize } = pageInfo;
     const diff = cntPerPage - remainder;
-    const inPageReverse = cntPerPage - inPageIndex; 
-    return ((pageInfo.totalPageSize -2 ) * pageInfo.cntPerPage) + inPageReverse + diff;  
+    const inPageReverse = cntPerPage - inPageIndex;
+    return ((pageInfo.totalPageSize - 2) * pageInfo.cntPerPage) + inPageReverse + diff;
 }
 
 export const useHomepageServerSide = async () => {
@@ -19,15 +19,33 @@ export const useHomepageServerSide = async () => {
         cache: "reload",
     })
 
-    const {Homepage:{data}} = await graphQLClient.request<homepage>(HOMEPAGE)
-    if(!data) throw Error("homepage is not exsist");
+    const defaultHomePage = {
+        PrivacyPolicy: "",
+        __typename: "Homepage",
+        blacklist: [],
+        logi: "",
+        loginOutRedirect: "",
+        loginRedirect: "",
+        marketingPolic: "",
+        partnerBpolicy: "",
+        partnerPolicy: "",
+        signUpRedirect: "",
+        siteDesc: "",
+        siteKeyWards: "",
+        siteName: "",
+        thirdPolicy: "",
+        travelerPolicy: "",
+        usePolicy: ""
+    }
+
+    const { Homepage: { data = defaultHomePage } } = await graphQLClient.request<homepage>(HOMEPAGE)
     console.log(data);
     return { data };
 }
 
-export const getStaticPageInfo = (key:TPageKeys) => async () => {
+export const getStaticPageInfo = (key: TPageKeys) => async () => {
     const { data } = await usePageInfo(key);
-    const {data: homepage} = await useHomepageServerSide();
+    const { data: homepage } = await useHomepageServerSide();
     return {
         revalidate: 1,
         props: {
@@ -38,7 +56,7 @@ export const getStaticPageInfo = (key:TPageKeys) => async () => {
 }
 
 
-export const getHomepage = async  () => {
+export const getHomepage = async () => {
     const homepage = await useHomepageServerSide();
     return {
         revalidate: 10,
