5cfa6e468269ebdbb7e112cea203c8b28b69f74e
JANDA-Developers <#janda123>
2021-01-04 19:49:23 +0900

    ..

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,9 +1,10 @@
-const END_POINT = "http://localhost:4000" + "/graphql";
+const END_POINT = "http://13.209.234.84:4000" + "/graphql";
 export const SERVER_URI: string = END_POINT || "";
+
 // "http://localhost:4000/graphql"
 // 프로덕트: http://13.209.234.84:4000/graphql
 // 개발: http://3.35.164.252:4000/graphql
 
 export default (() => {
-    return SERVER_URI;
+    return SERVER_URI;2
 })();
