a4c24876548d42e7b401fa0f2407ee48a08d9b42
JANDA-Developers <#janda123>
2021-01-22 00:58:57 +0900

    URI 수정

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,5 +1,5 @@
 const LOCAL = "http://localhost:4000"; 
-const PROD = "http://13.209.234.84:4000"; 
+const PROD = "https://pink.stayjanda.cloud"; 
 const DEV = "http://3.35.164.252:4000";
  
 export const SEVER = PROD;
