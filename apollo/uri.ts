412ba64ac775e9508c1198f941de8903bf783807
JANDA-Developers <#janda123>
2021-02-08 17:47:01 +0900

    .

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,9 +1,11 @@
 const PROD = "https://api.itsguide.co.kr";
 const LOCAL = "http://localhost:4000";
+
 export const SERVER = PROD;
 
 const END_POINT = PROD + "/graphql";
 export const SERVER_URI: string = END_POINT || "";
+
 
 export default (() => {
     return SERVER_URI;
