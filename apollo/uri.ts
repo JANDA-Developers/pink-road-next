a231eab641b016fdd82064548c0e6c939c2dc88f
JANDA-Developers <#janda123>
2021-03-03 19:12:17 +0900

    endpoint 수정

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,7 +1,8 @@
 const LOCAL = "http://localhost:4000"; 
-const PROD = "https://pink.stayjanda.cloud"; 
+const PROD = "https://api.itsguide.co.kr"; 
 const DEV = "http://3.35.164.252:4000";
  
+
 export const SEVER = process.env.NODE_ENV === "production" ? PROD : LOCAL;
 export const END_POINT = SEVER + "/graphql";
 export const SERVER_URI: string = END_POINT || "";
