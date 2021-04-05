21b800173c44408cc4460a6bf300e73b7d1c9ba9
ksh_dd <zocbdjf@gmail.com>
2021-04-05 17:05:00 +0900

    4/5 통합검색 디자인 보완

--- components/list/BoardListBlock.tsx
+++ components/list/BoardListBlock.tsx
@@ -12,8 +12,13 @@ interface IProp {
     board: BoardList_BoardList_data
 }
 
+function stripHtml(html: string) {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+}
+
 export const BoardListBlock: React.FC<IProp> = ({ board }) => {
-    const { _id, images, boardType, status, maxMember, adult_price, address, title, subTitle, thumb, keyWards, createdAt } = board;
+    const { _id, images, boardType, status, maxMember, adult_price, address, title, contents, thumb, keyWards, createdAt } = board;
     const router = useRouter();
 
     const getPath = () => {
@@ -37,9 +42,13 @@ export const BoardListBlock: React.FC<IProp> = ({ board }) => {
             style={BG(thumbImg || "")}
         />}
         <div className="textbox__01">
-            <div className="day">[ {yyyymmdd(createdAt)} ]</div>
+            <div className="textbox__01_top">
+                <div className="location">공지사항</div>
+                <div className="day">{yyyymmdd(createdAt)}</div>
+            </div>
+
             <div className="title">{title}</div>
-            <div className="txt">{subTitle}</div>
+            <div className="txt">{stripHtml(contents).substr(0, 200)}...</div>
             <div className="tag">
                 {keyWards?.map((keyward, index) =>
                     <Link href={generateSearchLink({ keyward })} key={_id + index + "keyward"}><a>#{keyward}</a></Link>
