b5f5516bb1a73b39323a3b5eaf82efa30137c2d4
ksh_dd <zocbdjf@gmail.com>
2021-04-21 17:20:00 +0900

    4/21 디자인수정

--- info/krPolicy.json.ts
+++ info/krPolicy.json.ts
@@ -1,9 +1,9 @@
 const krPolicyJson = {
     subTop_title: {
-        kr: "여행상품 계획하기"
+        kr: "상품등록"
     },
     subTop_desc: {
-        kr: "지금 여행을 떠나세요~!~~!!!!!"
+        kr: "투어, 체험 상품을 등록하세요. 여행상품을 심사를 거친후에 판매게시가 됩니다."
     },
     subTop_img: {
         kr: "/img/pr_img_06.jpg"
