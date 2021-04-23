7ab8600476fd1b30840040889f946eae913a8971
JANDA-Developers <#janda123>
2021-04-23 17:39:17 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,174 +1,212 @@
-import React, { useEffect, useState } from 'react';
-import 'css/all.css';
-import "dayjs/locale/ko"
-import dayjs from 'dayjs';
-import Layout from '../layout/Layout';
-import { ApolloProvider, useQuery } from '@apollo/client';
-import { getContext_GetProfile_data as IProfile, categoryList_CategoryList_data, getContext, UserRole, Fhomepage, Fcategory, CategoryType } from 'types/api';
-import PinkClient from "apollo/client"
-import { ALLOW_ADMINS, ALLOW_FULLESS, ALLOW_LOGINED, ALLOW_SELLERS } from '../types/const';
-import { GET_CONTEXT } from '../apollo/gql/queries';
-import PageDeny from './Deny';
-import { categoryMap, defaultCatsMap } from '../utils/categoryMap';
-import { Router, useRouter } from 'next/router';
-import PageLoading from './Loading';
-import { arrayEquals } from '../utils/filter';
+import React, { useEffect, useState } from "react";
+import "css/all.css";
+import "dayjs/locale/ko";
+import dayjs from "dayjs";
+import Layout from "../layout/Layout";
+import { ApolloProvider, useQuery } from "@apollo/client";
+import {
+    getContext_GetProfile_data as IProfile,
+    categoryList_CategoryList_data,
+    getContext,
+    UserRole,
+    Fhomepage,
+    Fcategory,
+    CategoryType,
+} from "types/api";
+import PinkClient from "apollo/client";
+import {
+    ALLOW_ADMINS,
+    ALLOW_FULLESS,
+    ALLOW_LOGINED,
+    ALLOW_SELLERS,
+} from "../types/const";
+import { GET_CONTEXT } from "../apollo/gql/queries";
+import PageDeny from "./Deny";
+import { categoryMap, defaultCatsMap } from "../utils/categoryMap";
+import { Router, useRouter } from "next/router";
+import PageLoading from "./Loading";
+import { arrayEquals } from "../utils/filter";
 import "slick-carousel/slick/slick.css";
-import useRouterScroll from '../hook/useRouterScroll';
-import { pageLoadingEffect } from '../utils/query';
+import useRouterScroll from "../hook/useRouterScroll";
+import { pageLoadingEffect } from "../utils/query";
 
+Router.events.on("routeChangeStart", () => {
+    pageLoadingEffect(true);
+});
+Router.events.on("routeChangeComplete", () => {
+    pageLoadingEffect(false);
+});
+Router.events.on("routeChangeError", () => {
+    pageLoadingEffect(false);
+});
 
-
-Router.events.on('routeChangeStart', () => { pageLoadingEffect(true) });
-Router.events.on('routeChangeComplete', () => { pageLoadingEffect(false) });
-Router.events.on('routeChangeError', () => { pageLoadingEffect(false) });
-
-
-dayjs.locale('ko')
+dayjs.locale("ko");
 
 export type TContext = {
-  categories: categoryList_CategoryList_data[]
-  role: UserRole
-  isAdmin: boolean,
-  isSeller: boolean,
-  isManager: boolean,
-  myProfile?: IProfile
-  isLogin?: boolean;
-  isParterB?: boolean;
-  homepage?: Fhomepage;
-  isParterNonB?: boolean;
-  categoriesMap: Record<CategoryType, Fcategory[]>
-  productGroupList: TProductGrop[]
-}
-
+    categories: categoryList_CategoryList_data[];
+    role: UserRole;
+    isAdmin: boolean;
+    isSeller: boolean;
+    isManager: boolean;
+    myProfile?: IProfile;
+    isLogin?: boolean;
+    isParterB?: boolean;
+    homepage?: Fhomepage;
+    isParterNonB?: boolean;
+    categoriesMap: Record<CategoryType, Fcategory[]>;
+    productGroupList: TProductGrop[];
+};
 
 export type TProductGrop = {
-  _id: string;
-  label: string;
-  groupCode: string;
-}
+    _id: string;
+    label: string;
+    groupCode: string;
+};
 
 const defaultContext: TContext = {
-  categories: [],
-  role: UserRole.anonymous,
-  isAdmin: false,
-  isManager: false,
-  isSeller: false,
-  myProfile: undefined,
-  homepage: undefined,
-  isLogin: false,
-  isParterB: false,
-  isParterNonB: false,
-  categoriesMap: defaultCatsMap,
-  productGroupList: []
-}
+    categories: [],
+    role: UserRole.anonymous,
+    isAdmin: false,
+    isManager: false,
+    isSeller: false,
+    myProfile: undefined,
+    homepage: undefined,
+    isLogin: false,
+    isParterB: false,
+    isParterNonB: false,
+    categoriesMap: defaultCatsMap,
+    productGroupList: [],
+};
 
 export const AppContext = React.createContext<TContext>(defaultContext);
 
 //APp파일은 서버사이드 렌더링만함
 function App({ Component, pageProps }: any) {
-  const [editMode, setEditMode] = useState(false);
-  const router = useRouter()
+    const [editMode, setEditMode] = useState(false);
+    const router = useRouter();
 
-  useRouterScroll();
+    useRouterScroll();
 
-  const ComponentLayout = Component.Layout ? Component.Layout : Layout;
-  const ComponentAuth = Component.Auth ? Component.Auth : ALLOW_FULLESS;
+    const ComponentLayout = Component.Layout ? Component.Layout : Layout;
+    const ComponentAuth = Component.Auth ? Component.Auth : ALLOW_FULLESS;
 
-  const { data, loading } = useQuery<getContext>(GET_CONTEXT, {
-    client: PinkClient,
-    nextFetchPolicy: "cache-first"
-  })
+    const { data, loading } = useQuery<getContext>(GET_CONTEXT, {
+        client: PinkClient,
+        nextFetchPolicy: "cache-first",
+    });
 
-  const homepage = data?.Homepage.data || undefined;
-  const catList = data?.CategoryList?.data || []
-  const myProfile = data?.GetProfile?.data || undefined
-  const role: UserRole = myProfile?.role || UserRole.anonymous
-  const productList = data?.GetProfile.data?.products.map(p => ({
-    _id: p._id,
-    label: p.title,
-    groupCode: p.groupCode
-  }))
+    const homepage = data?.Homepage.data || undefined;
+    const catList = data?.CategoryList?.data || [];
+    const myProfile = data?.GetProfile?.data || undefined;
+    const role: UserRole = myProfile?.role || UserRole.anonymous;
+    const productList = data?.GetProfile.data?.products.map((p) => ({
+        _id: p._id,
+        label: p.title,
+        groupCode: p.groupCode,
+    }));
 
-  const productGroupList: {
-    _id: string;
-    label: string;
-    groupCode: string;
-  }[] = [];
+    const productGroupList: {
+        _id: string;
+        label: string;
+        groupCode: string;
+    }[] = [];
 
-  productList?.forEach((p) => {
-    if (!productGroupList.find(g => g.groupCode === p.groupCode)) {
-      productGroupList.push({
-        ...p,
-      })
+    productList?.forEach((p) => {
+        if (!productGroupList.find((g) => g.groupCode === p.groupCode)) {
+            productGroupList.push({
+                ...p,
+            });
+        }
+    });
+
+    const isSeller = [
+        UserRole.partner,
+        UserRole.partnerB,
+        UserRole.manager,
+        UserRole.admin,
+    ].includes(role);
+    const isParterB = [
+        UserRole.partnerB,
+        UserRole.manager,
+        UserRole.admin,
+    ].includes(role);
+    const isParterNonB = [
+        UserRole.partner,
+        UserRole.manager,
+        UserRole.admin,
+    ].includes(role);
+    {
+        /* <DaumPostcode autoResize autoClose onSearch={() => { }} onComplete={(asd) => { }} /> */
     }
-  })
 
-  const isSeller = [UserRole.partner, UserRole.partnerB, UserRole.manager, UserRole.admin].includes(role);
-  const isParterB = [UserRole.partnerB, UserRole.manager, UserRole.admin].includes(role);
-  const isParterNonB = [UserRole.partner, UserRole.manager, UserRole.admin].includes(role);
-  {/* <DaumPostcode autoResize autoClose onSearch={() => { }} onComplete={(asd) => { }} /> */ }
+    const catsMap = categoryMap(catList);
 
-  const catsMap = categoryMap(catList);
-
-  if (data) {
-    const error = data.GetProfile.error
-  }
-
-
-  if (!ComponentAuth.includes(role || null) && !loading) {
-    if (arrayEquals(ComponentAuth, ALLOW_LOGINED)) {
-      if (loading) return;
-      Component = () => <PageDeny redirect="/login" msg="해당 페이지는 로그인후 이용 가능합니다." />
-      return <Component />;
-    } else {
-      if (loading) return;
-      Component = () => <PageDeny />
+    if (data) {
+        const error = data.GetProfile.error;
     }
-  }
 
-  if (
-    //인증 받지 않았으며 일반 권한은 아닌경우
-    ALLOW_SELLERS.includes(role) &&
-    !myProfile?.isVerifiedManager &&
-    !ComponentAuth.includes(UserRole.anonymous) &&
-    !ComponentAuth.includes(UserRole.individual)
-  ) {
-    Component = () => <PageDeny msg="인증되지 않은 판매자 입니다. 인증 소요시간은 평균 24시간 입니다." />
-  }
+    if (!ComponentAuth.includes(role || null) && !loading) {
+        if (arrayEquals(ComponentAuth, ALLOW_LOGINED)) {
+            if (loading) return;
+            Component = () => (
+                <PageDeny
+                    redirect="/login"
+                    msg="해당 페이지는 로그인후 이용 가능합니다."
+                />
+            );
+            return <Component />;
+        } else {
+            if (loading) return;
+            Component = () => <PageDeny />;
+        }
+    }
 
-  if (router.isFallback) {
-    return <div></div>
-  }
+    if (
+        //인증 받지 않았으며 일반 권한은 아닌경우
+        ALLOW_SELLERS.includes(role) &&
+        !myProfile?.isVerifiedManager &&
+        !ComponentAuth.includes(UserRole.anonymous) &&
+        !ComponentAuth.includes(UserRole.individual)
+    ) {
+        Component = () => (
+            <PageDeny msg="인증되지 않은 판매자 입니다. 인증 소요시간은 평균 24시간 입니다." />
+        );
+    }
 
-  if (loading) return <PageLoading />
-  return (
-    <div className="App">
-      <div id="MuPageLoading" className="muPageLoading" />
+    if (router.isFallback) {
+        return <div></div>;
+    }
 
-      <ApolloProvider client={PinkClient}>
-        <AppContext.Provider value={{
-          categoriesMap: catsMap,
-          categories: catList || [],
-          role,
-          myProfile,
-          isSeller,
-          isParterB,
-          isAdmin: role === UserRole.admin,
-          isManager: ALLOW_ADMINS.includes(role),
-          isLogin: !!myProfile,
-          isParterNonB,
-          homepage,
-          productGroupList,
-        }}>
-          <ComponentLayout>
-            <Component {...pageProps} />
-          </ComponentLayout>
-        </AppContext.Provider>
-      </ApolloProvider>
-      <div id="portal" />
-    </div>
-  );
+    if (loading) return <PageLoading />;
+    return (
+        <div className="App">
+            <div id="MuPageLoading" className="muPageLoading" />
+
+            <ApolloProvider client={PinkClient}>
+                <AppContext.Provider
+                    value={{
+                        categoriesMap: catsMap,
+                        categories: catList || [],
+                        role,
+                        myProfile,
+                        isSeller,
+                        isParterB,
+                        isAdmin: role === UserRole.admin,
+                        isManager: ALLOW_ADMINS.includes(role),
+                        isLogin: !!myProfile,
+                        isParterNonB,
+                        homepage,
+                        productGroupList,
+                    }}
+                >
+                    <ComponentLayout>
+                        <Component {...pageProps} />
+                    </ComponentLayout>
+                </AppContext.Provider>
+            </ApolloProvider>
+            <div id="portal" />
+        </div>
+    );
 }
 
 export default App;
--- utils/enumToKr.tsx
+++ utils/enumToKr.tsx
@@ -1,289 +1,288 @@
-<<<<<<< HEAD
-import { AddtionalFeesStatus, AnnounceType, bookingFindByCode_BookingFindByCode_data_bankTransInfo, BookingStatus, CategoryType, ERR_CODE, Fbooking, feePolicyFindOne_FeePolicyFindOne_data_addtionalFees, Fproduct, Fuser, GENDER, getContext_GetProfile_data, MethodType, NEWS_TYPE, PaymentStatus, PayMethod, ProductReOpenReq, ProductStatus, ProductType, QuestionStatus, RequestStatus, SettlementStatus, UserRole, UserStatus } from "../types/api";
-=======
 import {
-  AddtionalFeesStatus,
-  AnnounceType,
-  bookingFindByCode_BookingFindByCode_data_bankTransInfo,
-  BookingStatus,
-  CategoryType,
-  ERR_CODE,
-  Fbooking,
-  feePolicyFindOne_FeePolicyFindOne_data_addtionalFees,
-  Fproduct,
-  Fuser,
-  GENDER,
-  MethodType,
-  NEWS_TYPE,
-  PaymentStatus,
-  PayMethod,
-  ProductReOpenReq,
-  ProductStatus,
-  ProductType,
-  QuestionStatus,
-  RequestStatus,
-  SettlementStatus,
-  UserRole,
-  UserStatus,
+    AddtionalFeesStatus,
+    AnnounceType,
+    bookingFindByCode_BookingFindByCode_data_bankTransInfo,
+    BookingStatus,
+    CategoryType,
+    ERR_CODE,
+    Fbooking,
+    feePolicyFindOne_FeePolicyFindOne_data_addtionalFees,
+    Fproduct,
+    Fuser,
+    GENDER,
+    getContext_GetProfile_data,
+    MethodType,
+    NEWS_TYPE,
+    PaymentStatus,
+    PayMethod,
+    ProductReOpenReq,
+    ProductStatus,
+    ProductType,
+    QuestionStatus,
+    RequestStatus,
+    SettlementStatus,
+    UserRole,
+    UserStatus,
 } from "../types/api";
->>>>>>> origin/design
 
 export const bookingStatus = (status?: BookingStatus | null) => {
-  if (status === BookingStatus.CANCEL) return "예약취소";
-  if (status === BookingStatus.COMPLETE) return "예약완료";
-  if (status === BookingStatus.READY) return "예약대기";
-  return "";
+    if (status === BookingStatus.CANCEL) return "예약취소";
+    if (status === BookingStatus.COMPLETE) return "예약완료";
+    if (status === BookingStatus.READY) return "예약대기";
+    return "";
 };
 
 export const productStatus = (status?: ProductStatus | null) => {
-  if (status === ProductStatus.CANCELD) return "취소";
-  if (status === ProductStatus.COMPLETED) return "완료";
-  if (status === ProductStatus.EXPIRED) return "만료";
-  if (status === ProductStatus.OPEN) return "판매중";
-  if (status === ProductStatus.READY) return "생성요청";
-  if (status === ProductStatus.REFUSED) return "생성거절됨";
-  if (status === ProductStatus.UPDATE_REQ) return "업데이트요청";
-  if (status === ProductStatus.UPDATE_REQ_REFUSED) return "업데이트요청거절";
-  return status || "";
+    if (status === ProductStatus.CANCELD) return "취소";
+    if (status === ProductStatus.COMPLETED) return "완료";
+    if (status === ProductStatus.EXPIRED) return "만료";
+    if (status === ProductStatus.OPEN) return "판매중";
+    if (status === ProductStatus.READY) return "생성요청";
+    if (status === ProductStatus.REFUSED) return "생성거절됨";
+    if (status === ProductStatus.UPDATE_REQ) return "업데이트요청";
+    if (status === ProductStatus.UPDATE_REQ_REFUSED) return "업데이트요청거절";
+    return status || "";
 };
 
 export const paymentStatus = (status?: PaymentStatus | null) => {
-  if (status === PaymentStatus.CANCEL) return "환불완료";
-  if (status === PaymentStatus.COMPLETE) return "완료";
-  if (status === PaymentStatus.READY) return "대기중";
-  return "";
+    if (status === PaymentStatus.CANCEL) return "환불완료";
+    if (status === PaymentStatus.COMPLETE) return "완료";
+    if (status === PaymentStatus.READY) return "대기중";
+    return "";
 };
 
 export const paymentStatus2 = (obj?: any | null) => {
-  if (!obj) return "미결제";
-  const { status } = obj;
-  if (status === PaymentStatus.CANCEL) return "환불완료";
-  if (status === PaymentStatus.COMPLETE) return "완료";
-  if (status === PaymentStatus.READY) return "대기중";
-  return "";
+    if (!obj) return "미결제";
+    const { status } = obj;
+    if (status === PaymentStatus.CANCEL) return "환불완료";
+    if (status === PaymentStatus.COMPLETE) return "완료";
+    if (status === PaymentStatus.READY) return "대기중";
+    return "";
 };
 
 export const settlementStatus = (
-  status?: SettlementStatus | null,
-  productStatus?: ProductStatus | null
+    status?: SettlementStatus | null,
+    productStatus?: ProductStatus | null
 ) => {
-  if (productStatus && productStatus !== ProductStatus.COMPLETED)
-    return "요청불가";
-  if (status === SettlementStatus.REQUEST) return "요청";
-  if (status === SettlementStatus.COMPLETE) return "완료";
-  if (status === SettlementStatus.READY) return "요청가능";
-  if (status === SettlementStatus.CANCELED) return "취소";
-  return "";
+    if (productStatus && productStatus !== ProductStatus.COMPLETED)
+        return "요청불가";
+    if (status === SettlementStatus.REQUEST) return "요청";
+    if (status === SettlementStatus.COMPLETE) return "완료";
+    if (status === SettlementStatus.READY) return "요청가능";
+    if (status === SettlementStatus.CANCELED) return "취소";
+    return "";
 };
 
 export const foreginKR = (isForegin: boolean) => {
-  return isForegin ? "외국인" : "내국인";
+    return isForegin ? "외국인" : "내국인";
 };
 
 export const confirmKr = (flag: boolean) => {
-  return flag ? "승인" : "미승인";
+    return flag ? "승인" : "미승인";
 };
 
 export const questionSatus = (status: QuestionStatus) => {
-  if (status === QuestionStatus.COMPLETE) return "답변완료";
-  if (status === QuestionStatus.READY) return "미답변";
-  return "";
+    if (status === QuestionStatus.COMPLETE) return "답변";
+    if (status === QuestionStatus.READY) return "미답변";
+    return "";
 };
 
 export const itemTypeToKr = (type: ProductType) => {
-  if (type === ProductType.EXPERIENCE) return "체험";
-  if (type === ProductType.TOUR) return "체험";
-  return "";
+    if (type === ProductType.EXPERIENCE) return "체험";
+    if (type === ProductType.TOUR) return "체험";
+    return "";
 };
 
 export const determinedKr = (isDetermined: boolean) => {
-  if (isDetermined) return "출발확정";
-  if (!isDetermined) return "미확정";
-  return "";
+    if (isDetermined) return "출발확정";
+    if (!isDetermined) return "미확정";
+    return "";
 };
 
 export const categoryToKR = (catType?: CategoryType | null) => {
-  if (catType === CategoryType.QUESTION) return "문의글";
-  if (catType === CategoryType.PORTPOLIO) return "포트폴리오";
-  if (catType === CategoryType.QNA) return "자주하는 질문";
-  if (catType === CategoryType.TOUR) return "투어";
-  if (catType === CategoryType.EXPERIENCE) return "체험";
-  if (catType === CategoryType.REGION) return "지역";
-  return "";
+    if (catType === CategoryType.QUESTION) return "문의글";
+    if (catType === CategoryType.PORTPOLIO) return "포트폴리오";
+    if (catType === CategoryType.QNA) return "문의하기";
+    if (catType === CategoryType.TOUR) return "투어";
+    if (catType === CategoryType.EXPERIENCE) return "체험";
+    if (catType === CategoryType.REGION) return "지역";
+    return "";
 };
 
 export const payMethodToKR = (paymethod?: PayMethod) => {
-  if (paymethod === PayMethod.BANK) return "무통장입금";
-  if (paymethod === PayMethod.HAND) return "수기등록(무통장)";
-  if (paymethod === PayMethod.NICEPAY_CARD) return "카드결제";
-  return "";
+    if (paymethod === PayMethod.BANK) return "무통장입금";
+    if (paymethod === PayMethod.HAND) return "수기등록(무통장)";
+    if (paymethod === PayMethod.NICEPAY_CARD) return "카드결제";
+    return "";
 };
 
 export const userRoleToKR = (role?: UserRole) => {
-  if (role === UserRole.partner) return "파트너";
-  if (role === UserRole.partnerB) return "기업파트너";
-  if (role === UserRole.individual) return "개인회원";
-  if (role === UserRole.manager) return "마스터";
-  if (role === UserRole.admin) return "관리자(JANDA)";
-  return "";
+    if (role === UserRole.partner) return "파트너";
+    if (role === UserRole.partnerB) return "기업파트너";
+    if (role === UserRole.individual) return "개인회원";
+    if (role === UserRole.manager) return "마스터";
+    if (role === UserRole.admin) return "관리자(JANDA)";
+    return "";
 };
 
 export const genderToKR = (gender?: GENDER | null) => {
-  if (gender === GENDER.FEMALE) return "여성";
-  if (gender === GENDER.MAIL) return "남성";
-  return "";
+    if (gender === GENDER.FEMALE) return "여성";
+    if (gender === GENDER.MAIL) return "남성";
+    return "";
 };
 
 export function newTypeToKr(type?: any) {
-  if (type === NEWS_TYPE.CULTURE) return "문화이야기";
-  if (type === NEWS_TYPE.MEDIA) return "언론보도";
-  if (type === NEWS_TYPE.TRAVEL) return "여행이야기";
-  return "";
+    if (type === NEWS_TYPE.CULTURE) return "문화이야기";
+    if (type === NEWS_TYPE.MEDIA) return "언론보도";
+    if (type === NEWS_TYPE.TRAVEL) return "여행이야기";
+    return "";
 }
 
 export const announceTypeKR = (type?: AnnounceType) => {
-  if (type === AnnounceType.ACCOUNCE) return "공지";
-  if (type === AnnounceType.NOICE) return "알림";
-  return "";
+    if (type === AnnounceType.ACCOUNCE) return "공지";
+    if (type === AnnounceType.NOICE) return "알림";
+    return "";
 };
 
 export const feePresent = (
-  addFee: feePolicyFindOne_FeePolicyFindOne_data_addtionalFees
+    addFee: feePolicyFindOne_FeePolicyFindOne_data_addtionalFees
 ) => {
-  let unit = addFee.type === AddtionalFeesStatus.DEFAULT ? "(원)" : "(%)";
-  let amt =
-    addFee.type === AddtionalFeesStatus.DEFAULT
-      ? addFee.fee
-      : addFee.feePercent;
-  return amt + unit;
+    let unit = addFee.type === AddtionalFeesStatus.DEFAULT ? "(원)" : "(%)";
+    let amt =
+        addFee.type === AddtionalFeesStatus.DEFAULT
+            ? addFee.fee
+            : addFee.feePercent;
+    return amt + unit;
 };
 
 export const ErrorCode: Partial<Record<ERR_CODE, string>> = {
-  ALEADY_SAME_DATA: "중복 데이터가 존재합니다.",
-  AUTHORIZATION: "권한이 없습니다.",
-  BOOKING_MEMBER_OVER: "인원이 초과되었습니다.",
-  DOC_ALEADY_EXIST: "이미 존재하는 데이터 입니다.",
-  DOC_NOT_FOUND: "해당 데이터를 찾을 수 없습니다",
-  DOC_RELATED_INVALID: "잘못된 데이터 입니다.",
-  EXPECTED_STATUS_NOT: "잘못된 요청입니다.",
-  EXPECTED_VALUE_RANGE_NOT: "요청값이 올바르지 않습니다.",
-  INVALID_PARAMS: "잘못된 요청입니다.",
-  NICKNAME_ALEADY_EXIST: "닉네임",
-  PAY_TIME_OVER: "시간이 경과했습니다.",
+    ALEADY_SAME_DATA: "중복 데이터가 존재합니다.",
+    AUTHORIZATION: "권한이 없습니다.",
+    BOOKING_MEMBER_OVER: "인원이 초과되었습니다.",
+    DOC_ALEADY_EXIST: "이미 존재하는 데이터 입니다.",
+    DOC_NOT_FOUND: "해당 데이터를 찾을 수 없습니다",
+    DOC_RELATED_INVALID: "잘못된 데이터 입니다.",
+    EXPECTED_STATUS_NOT: "잘못된 요청입니다.",
+    EXPECTED_VALUE_RANGE_NOT: "요청값이 올바르지 않습니다.",
+    INVALID_PARAMS: "잘못된 요청입니다.",
+    NICKNAME_ALEADY_EXIST: "닉네임",
+    PAY_TIME_OVER: "시간이 경과했습니다.",
 };
 
 export const errorMessage = (code?: ERR_CODE | string) => {
-  const result = ErrorCode[code as ERR_CODE];
-  if (result) {
-    alert(result);
-  } else {
-  }
+    const result = ErrorCode[code as ERR_CODE];
+    if (result) {
+        alert(result);
+    } else {
+    }
 };
 
 export const managerVerifiedKR = (managerVerified?: boolean) => {
-  return managerVerified ? "승인됨" : "승인요청";
+    return managerVerified ? "승인됨" : "승인요청";
 };
 
 export const userStatusKR = (user?: Fuser) => {
-  if (user?.isResigned) return "탈퇴유저";
-  if (user?.isDenied) return "가입거절";
-  if (!user?.isVerifiedManager) return "가입요청";
-  if (user?.status === UserStatus.stop) return "활동정지";
-  if (user?.status === UserStatus.ok) return "활동중";
-  return "";
+    if (user?.isResigned) return "탈퇴유저";
+    if (user?.isDenied) return "가입거절";
+    if (!user?.isVerifiedManager) return "가입요청";
+    if (user?.status === UserStatus.stop) return "활동정지";
+    if (user?.status === UserStatus.ok) return "활동중";
+    return "";
 };
 
 export const requestStatusKr = (req?: RequestStatus) => {
-  if (req === RequestStatus.ACCEPT) return "수락";
-  if (req === RequestStatus.CANCEL) return "취소";
-  if (req === RequestStatus.CANCEL_REQ) return "취소요청";
-  if (req === RequestStatus.COMPLETE) return "완료";
-  if (req === RequestStatus.DETERMIN) return "출발결정";
-  if (req === RequestStatus.EXPIRED) return "만료";
-  if (req === RequestStatus.REJECT) return "거절";
-  if (req === RequestStatus.REQUEST) return "요청";
-  if (req === RequestStatus.WITHDRAWAL) return "출발철회";
-  return "";
+    if (req === RequestStatus.ACCEPT) return "수락";
+    if (req === RequestStatus.CANCEL) return "취소";
+    if (req === RequestStatus.CANCEL_REQ) return "취소요청";
+    if (req === RequestStatus.COMPLETE) return "완료";
+    if (req === RequestStatus.DETERMIN) return "출발결정";
+    if (req === RequestStatus.EXPIRED) return "만료";
+    if (req === RequestStatus.REJECT) return "거절";
+    if (req === RequestStatus.REQUEST) return "요청";
+    if (req === RequestStatus.WITHDRAWAL) return "출발철회";
+    return "";
 };
 
 export const methodTypeKr = (mt?: MethodType) => {
-  if (mt === MethodType.BOOKING) return "예약";
-  if (mt === MethodType.PRODUCT) return "상품";
-  if (mt === MethodType.PRODUCT_CREATE) return "상품생성";
-  if (mt === MethodType.PRODUCT_REOPEN) return "상품재개";
-  if (mt === MethodType.SETTLEMENT) return "정산";
-  if (mt === MethodType.TRAVEL) return "여행";
-  return "";
+    if (mt === MethodType.BOOKING) return "예약";
+    if (mt === MethodType.PRODUCT) return "상품";
+    if (mt === MethodType.PRODUCT_CREATE) return "상품생성";
+    if (mt === MethodType.PRODUCT_REOPEN) return "상품재개";
+    if (mt === MethodType.SETTLEMENT) return "정산";
+    if (mt === MethodType.TRAVEL) return "여행";
+    return "";
 };
 
 export const userStatusReverseKR = (user?: Fuser) => {
-  if (user?.isDenied) return "가입승인";
-  if (!user?.isVerifiedManager) return "가입거절";
-  return "";
+    if (user?.isDenied) return "가입승인";
+    if (!user?.isVerifiedManager) return "가입거절";
+    return "";
 };
 
 export const isOpenKr = (isOpen?: boolean) => {
-  if (isOpen) return "공개";
-  if (!isOpen) return "비공개";
-  return "";
+    if (isOpen) return "공개";
+    if (!isOpen) return "비공개";
+    return "";
 };
 
 export const reqToKr = (req?: ProductReOpenReq | null) => {
-  if (req === ProductReOpenReq.REOPEN) return "오픈요청";
-  return "";
+    if (req === ProductReOpenReq.REOPEN) return "오픈요청";
+    return "";
 };
 
 interface IReqBadgeProp {
-  req?: ProductReOpenReq | null;
+    req?: ProductReOpenReq | null;
 }
 
 export const ReqBadge: React.FC<IReqBadgeProp> = ({ req }) => {
-  if (req === ProductReOpenReq.REOPEN)
-    return <span>{`[${reqToKr(req)}]`}</span>;
-  return <span />;
+    if (req === ProductReOpenReq.REOPEN)
+        return <span>{`[${reqToKr(req)}]`}</span>;
+    return <span />;
 };
 
 interface IBookingInfo {}
 
 export const personCountBracket = (info: Fbooking) => {
-  const { adultCount, babyCount, kidCount } = info;
+    const { adultCount, babyCount, kidCount } = info;
 
-  return `( 성인${adultCount} / 소아${kidCount} / 유아${babyCount} )`;
+    return `( 성인${adultCount} / 소아${kidCount} / 유아${babyCount} )`;
 };
 
 export const peopleCurrentCountBracket = (info: Fproduct) => {
-  const { peopleCount, maxMember, minMember } = info;
+    const { peopleCount, maxMember, minMember } = info;
 
-  return `(인원:${peopleCount} / 최소${minMember} / 최대${maxMember})`;
+    return `(인원:${peopleCount} / 최소${minMember} / 최대${maxMember})`;
 };
 interface Author {
-  name: string;
-  busi_name: string;
-  blueBird?: number;
-  [key: string]: any;
+    name: string;
+    busi_name: string;
+    blueBird?: number;
+    [key: string]: any;
 }
+
 export const withCompany = (info?: Author | null) => {
-  const { name, busi_name, blueBird } = info || {};
-  return `${name}${busi_name ? `(${busi_name})` : ""}${
-    blueBird ? `(파랑새${blueBird}기)` : ""
-  }`;
+    const { name, busi_name, blueBird } = info || {};
+    return `${name}${busi_name ? `(${busi_name})` : ""}${
+        blueBird ? `(파랑새${blueBird}기)` : ""
+    }`;
 };
 
 interface Author {}
 export const bankrefundTransInfo = (
-  bankTransInfo?: bookingFindByCode_BookingFindByCode_data_bankTransInfo
+    bankTransInfo?: bookingFindByCode_BookingFindByCode_data_bankTransInfo
 ) => {
-  const { accountHolder, accountNumber, bankName } = bankTransInfo || {};
-  return `${accountHolder} ${accountNumber}(${bankName})`;
+    const { accountHolder, accountNumber, bankName } = bankTransInfo || {};
+    return `${accountHolder} ${accountNumber}(${bankName})`;
 };
-<<<<<<< HEAD
 
-export const nameOf = (myProfile?: any) => {
-    console.log({myProfile})
-    return myProfile?.name || myProfile?.busi_name ||  myProfile?.manageName || "";
-}
-export const phoneNumberOf = (myProfile?: getContext_GetProfile_data | null) => {
-    return myProfile?.manageContact || myProfile?.phoneNumber || myProfile?.busi_contact || ""
-}
-=======
->>>>>>> origin/design
+export const nameOf = (author?: any) => {
+    return author?.manageName || author?.name || author?.busi_name || "";
+};
+export const phoneNumberOf = (author?: getContext_GetProfile_data | null) => {
+    return (
+        author?.manageContact ||
+        author?.phoneNumber ||
+        author?.busi_contact ||
+        ""
+    );
+};
